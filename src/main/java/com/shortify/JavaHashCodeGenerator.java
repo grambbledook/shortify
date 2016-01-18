65c0bd1a0761ed7f11c10c9330fecd2c9ba77e07
Denis Vodopyanov <dvodopyanov@griddynamics.com>
2016-01-18 18:44:40 +0300

    JavaHashCodeGenerator fixed to use variable of type long instead of int for hash code generation

--- src/main/java/com/shortify/JavaHashCodeGenerator.java
+++ src/main/java/com/shortify/JavaHashCodeGenerator.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Component;
 public class JavaHashCodeGenerator implements HashCodeGenerator {
     @Override
     public long generateHashCode(String string) {
-        return string.hashCode();
+        long hash = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            hash = 31 * hash + string.charAt(i);
+        }
+
+        return hash;
     }
 }
